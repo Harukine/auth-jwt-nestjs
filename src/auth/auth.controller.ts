a42ae1893fa12b82a0b4dec335e8a682b445390d
Jacob Nguyen <jacobhtnguyen@gmail.com>
2022-09-22 16:39:18 -0400

    created endpoint stubs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/local/signup')
+  signupLocal() {
+    this.authService.signinLocal();
+  }
+
+  @Post('/local/signin')
+  signinLocal() {
+    this.authService.signinLocal();
+  }
+
+  @Post('/logout')
+  logout() {
+    this.authService.logout();
+  }
+
+  @Post('/refresh')
+  refresh() {
+    this.authService.refresh();
+  }
+}
