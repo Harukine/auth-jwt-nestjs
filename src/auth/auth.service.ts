bd2002418b42c94fcd5a28897dc8bffabd8c0294
Jacob Nguyen <jacobhtnguyen@gmail.com>
2022-09-22 17:38:20 -0400

    implemented basic signup endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  signupLocal(dto: AuthDto) {
+    const newUser = this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash: dto.password,
+      },
+    });
+  }
+
+  signinLocal() {}
+
+  logout() {}
+
+  refresh() {}
+}
