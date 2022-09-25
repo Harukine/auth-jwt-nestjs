6fe5c0bcb74a6c68c90618e70e90e23b8d4e2ef9
Jacob Nguyen <jacobhtnguyen@gmail.com>
2022-09-25 16:13:57 -0400

    implemented function to return tokens

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,23 +2,57 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
+import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
   hashData(data: string) {
     return bcrypt.hash(data, 10);
   }
 
-  async signupLocal(dto: AuthDto) {
+  async getTokens(userId: number, email: string): Promise<Tokens> {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'at-secret', // TODO should be in configs instead of hardcode
+          expiresIn: 60 * 15,
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'rt-secret', // TODO should be in configs instead of hardcode
+          expiresIn: 60 * 60 * 24 * 7,
+        },
+      ),
+    ]);
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
+  }
+
+  async signupLocal(dto: AuthDto): Promise<Tokens> {
     const hash = await this.hashData(dto.password);
-    const newUser = this.prisma.user.create({
+
+    const newUser = await this.prisma.user.create({
       data: {
         email: dto.email,
         hash,
       },
     });
+
+    return await this.getTokens(newUser.id, newUser.email);
   }
 
   signinLocal() {}
