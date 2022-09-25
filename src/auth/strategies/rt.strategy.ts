83363acd2d3c33981bc3c38f10751a2d18cd2af5
Jacob Nguyen <jacobhtnguyen@gmail.com>
2022-09-25 15:57:41 -0400

    created passport strategies

---
+++ src/auth/strategies/rt.strategy.ts
@@ -0,0 +1,19 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'rt-secret', // should be in configs instead of hardcode
+      passReqToCallback: true,
+    });
+  }
+
+  validate(req: Request, payload: any) {
+    return payload;
+  }
+}
