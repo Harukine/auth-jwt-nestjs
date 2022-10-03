dff07b0a13e3b2b8c60b5cd4cd34c48bd0c219c9
Jacob Nguyen <jacobhtnguyen@gmail.com>
2022-10-03 16:27:34 -0400

    applied public decorators to public routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,32 +9,34 @@ import {
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Tokens } from './types';
-import { AtGuard, RtGuard } from '../guards';
-import { GetCurrentUser, GetCurrentUserId } from '../decorators';
+import { RtGuard } from '../guards';
+import { GetCurrentUser, GetCurrentUserId, Public } from '../decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('/local/signup')
   @HttpCode(HttpStatus.CREATED)
   signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
     return this.authService.signupLocal(dto);
   }
 
+  @Public()
   @Post('/local/signin')
   @HttpCode(HttpStatus.OK)
   signinLocal(@Body() dto: AuthDto): Promise<Tokens> {
     return this.authService.signinLocal(dto);
   }
 
-  @UseGuards(AtGuard)
   @Post('/logout')
   @HttpCode(HttpStatus.OK)
   logout(@GetCurrentUserId() userId: number): Promise<void> {
     return this.authService.logout(userId);
   }
 
+  @Public()
   @UseGuards(RtGuard)
   @Post('/refresh')
   @HttpCode(HttpStatus.OK)
