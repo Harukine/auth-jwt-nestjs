cf879ead5bcf348e9e73fa7681ca203aeb6a0894
Jacob Nguyen <jacobhtnguyen@gmail.com>
2022-10-17 14:30:48 -0400

    updated logout handler to return boolean

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -32,7 +32,7 @@ export class AuthController {
 
   @Post('/logout')
   @HttpCode(HttpStatus.OK)
-  logout(@GetCurrentUserId() userId: number): Promise<void> {
+  logout(@GetCurrentUserId() userId: number): Promise<boolean> {
     return this.authService.logout(userId);
   }
 
